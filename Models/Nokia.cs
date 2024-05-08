8157c7cd8665cf8b92ab1968a9c7b6a6d50bd365
wictor luciano <wluciano01@gmail.com>
2024-05-07 23:04:51 -0300

    Update challange

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("instalando app: " + nomeApp);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Infomações do Smartphone Nokia: ");
+
+Nokia nokia1 = new Nokia(numero: "21966993198", modelo: "Xperia 3000", imei: "221133447788", memoria: 68);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Wpp");
+
+Console.WriteLine(nokia1.Numero);
+
+Console.WriteLine("Infomações do Smartphone Iphone: ");
+
+Nokia iphone1 = new Nokia(numero: "21966993198", modelo: "Xperia 3000", imei: "221133447788", memoria: 68);
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Wpp");
+
+Console.WriteLine(iphone1.Numero);
